441c214303f47c77bc4de240924209fa7b464597
JosephCalebAnderson <jca47319@uga.edu>
2024-02-15 19:21:12 -0500

    Player Vs Player is set up and working.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [selectedStones, setSelectedSeats] = useState([]);
+  // Needed values for implementation
+  const [selectedStones, setSelectedStones] = useState([]);
   const [removedStones, setRemovedStones] = useState([]);
-  const [numStonesSelected, setNumSeatsSelected] = useState(0);
-
+  const [numStonesSelected, setNumStonesSelected] = useState(0);
+  const [turnPrompt, setTurnPrompt] = useState("Player 1");
+  const [isPlayer1Turn, setIsPlayer1Turn] = useState(true);
+  const [isGameOver, setIsGameOver] = useState(false);
   const stoneElements = [];
-  const size = 10;
+  // Change this to change the size of the circle
+  const size = 4;
+  var gameOver = false;
 
   for (let stoneNum = 1; stoneNum <= size; stoneNum++) {
     const stoneId = `${stoneNum}`;
@@ -18,7 +23,7 @@ function App() {
       
     stoneElements.push(
       <btn
-        style = {{ position: 'absolute', top: window.innerHeight/2 + 300 * Math.sin(stoneNum*2*Math.PI/size) - 75, right: window.innerWidth/2 + 300 * Math.cos(stoneNum*2*Math.PI/size) - 75}}
+        style = {{ position: 'absolute', top: window.innerHeight/2 + 300 * Math.sin(stoneNum*2*Math.PI/size) - 50, right: window.innerWidth/2 + 300 * Math.cos(stoneNum*2*Math.PI/size) - 50}}
         className={`stone ${isStoneAvailable ? (isStoneSelected ? 'selectedStone' : '') : 'takenStone'}`}
         key={stoneId}
         onClick={() => handleStoneClick(stoneId)}
@@ -32,23 +37,53 @@ function App() {
     const isAvailable = !removedStones.includes(stoneId);
 
     if (!isSelected && isAvailable && numStonesSelected < 2) {
-      setSelectedSeats([...selectedStones, stoneId]);
-      setNumSeatsSelected(numStonesSelected + 1);
+      // Okay if the stone selected is right next to the one being selected.
+      if (selectedStones.length == 0 || 
+        Math.abs(selectedStones[0] - stoneId) == 1 ||
+        // The following are two special cases to allow the ends of the circle to be taken at the same time. 
+        selectedStones[0] == size && stoneId == 1 ||
+        selectedStones[0] == 1 && stoneId == size) {
+        setSelectedStones([...selectedStones, stoneId]);
+        setNumStonesSelected(numStonesSelected + 1);
+      } else {
+        //Illegal move, can add error message here if needed.
+      }
     } else if (isSelected) {
       const updatedSeats = selectedStones.filter((seat) => seat !== stoneId);
-      setSelectedSeats(updatedSeats);
-      setNumSeatsSelected(numStonesSelected - 1);
+      setSelectedStones(updatedSeats);
+      setNumStonesSelected(numStonesSelected - 1);
     }
   };
 
-  function log() {
-    console.log("Clicked");
+  const handleSelectionConfirmation = async () => {
+    if (numStonesSelected > 0) {
+      setRemovedStones([...removedStones, ...selectedStones]);
+      setSelectedStones([]);
+      setNumStonesSelected(0);
+      if (turnPrompt.includes('2')) {
+        setTurnPrompt("Player 1");
+        setIsPlayer1Turn(true);
+      } else {
+        setTurnPrompt("Player 2");
+        setIsPlayer1Turn(false);
+      }
+    }
   }
+
+  useEffect(() => {
+    if (removedStones.length == size) {
+      gameOver = true;
+      setIsGameOver(true);
+    }
+  }, [removedStones]);
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="stones">
+        {isGameOver && <h2 className={`prompt ${isPlayer1Turn ?  'p1' : 'p2'}`}>{turnPrompt} Wins!</h2>}
+          {!isGameOver && <h3 className={`prompt ${isPlayer1Turn ?  'p1' : 'p2'}`}>{turnPrompt}'s Turn</h3>}
+          {!isGameOver && <btn className="selection" onClick = {() => handleSelectionConfirmation()}>Confirm Move</btn>}
           {stoneElements}
         </div>
       </header>
